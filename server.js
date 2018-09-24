77bb5179ad26f117e7c0c741798b6b89f85ac516
Windsvept <junsubjjang@gmail.com>
2018-09-23 19:36:12 -0700

    final

--- server.js
+++ server.js
@@ -12,13 +12,9 @@ app.use(morgan('dev'));
 app.use('/api/header', proxy('http://localhost:7763'));
 app.use('/api/photos', proxy('http://localhost:3001'));
 app.use('/api/sidebar', proxy('http://localhost:7878'));
-// app.use('/api', (req, res) => {
-//   app.get('/reviews/:id', proxy('http://localhost:3002'));
-// });
+
 app.get('/api/reviews/:id', (req, res) => {
-  console.log(req);
   let url = req.url.slice(4);
-  console.log(url);
   fetch(`http://localhost:3002${url}`)
   .then(response => response.json())
   .then(results => res.send(results));
